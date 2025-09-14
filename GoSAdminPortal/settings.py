9232e90f2446ca81eaccc43f317565a83be3ae98
Sarah Withee <2601974+geekygirlsarah@users.noreply.github.com>
2025-09-14 17:24:36 -0400

    Maybe fix db error?

--- GoSAdminPortal/settings.py
+++ GoSAdminPortal/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         'DEFAULT_AUTO_FIELD': 'django.db.models.AutoField'
     }
 }
-if os.getenv('DATABASE_URL'):
+if os.getenv('DATABASE_URL', None):
     DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
