e31d7a10dd478829102354d865b966125c29932b
Sarah Withee <gitlab-95c7f0d6@geekygirlsarah.com>
2026-07-03 18:20:08 -0400

    Attempt #3 to resolve psycopg install issue

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install -r requirements.txt
+#rm -rf appvenv
+#python3 -m venv appvenv
 
+#appvenv/bin/pip install --no-cache-dir -r requirements.txt
+python -m pip install --no-cache-dir -r requirements.txt
+
+#appvenv/bin/python manage.py collectstatic --no-input
+#appvenv/bin/python manage.py migrate
 python manage.py collectstatic --no-input
-
 python manage.py migrate
